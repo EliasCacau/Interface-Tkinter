bfa7d8a62102f655c15e8370f5b4a951146e79d8
Elias Cacau <81997464+EliasCacau@users.noreply.github.com>
2022-07-20 09:13:26 -0500

    Add files via upload

--- interface/exercicio/atividade_3.py
+++ interface/exercicio/atividade_3.py
@@ -7,19 +7,19 @@ class Tela:
         self.janela.geometry("400x230")
         self.janela.resizable(0, 0)
         self.but1 = tk.Button(self.janela, text='1')
-        self.but1.grid(column=5, row=0, stick=tk.N,  padx=3, pady=0, columnspan=3)
+        self.but1.grid(column=3, row=0, stick=tk.N,  padx=3, pady=0, rowspan=3)
 
         self.but2 = tk.Button(self.janela, text='2')
-        self.but2.grid(column=2, row=1,stick=tk.N, padx=0)
+        self.but2.grid(column=2, row=1,stick=tk.N, padx=0, rowspan=3)
 
         self.but3 = tk.Button(self.janela, text='3')
-        self.but3.grid(column=3, row=1,stick=tk.N, padx=0, pady=0)
+        self.but3.grid(column=3, row=1,stick=tk.N, padx=0, pady=0, rowspan=4)
 
         self.but4 = tk.Button(self.janela, text='4')
         self.but4.grid(column=0, row=2,stick=tk.N, padx=0, pady=0)
 
         self.but5 = tk.Button(self.janela, text='5')
-        self.but5.grid(column=2, row=2,stick=tk.N, padx=0, pady=0)
+        self.but5.grid(column=1, row=2,stick=tk.N, padx=0, pady=0)
 
         self.but6 = tk.Button(self.janela, text='6')
         self.but6.grid(column=2, row=2,stick=tk.N, padx=0, pady=0)
