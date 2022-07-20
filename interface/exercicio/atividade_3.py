3bcf6e59c4d3a8d1885749fc1c86c986577cb595
Elias Cacau <81997464+EliasCacau@users.noreply.github.com>
2022-07-20 17:23:49 -0500

    oi

--- interface/exercicio/atividade_3.py
+++ interface/exercicio/atividade_3.py
@@ -4,25 +4,28 @@ class Tela:
     def __init__(self, master):
         self.janela = master
         self.janela.title("Gerenciador Grid")
-        self.janela.geometry("200x180")
+        self.janela.geometry("300x200")
         self.janela.resizable(0, 0)
         self.but1 = tk.Button(self.janela, text='1', width="5", height="2")
-        self.but1.grid(column=1, row=0,   padx=0, pady=0)
+        self.but1.grid(column=2, row=0,   padx=0, pady=0)
 
         self.but2 = tk.Button(self.janela, text='2', width="5", height="2")
-        self.but2.grid(column=0, row=1, padx=0, columnspan=2)
+        self.but2.grid(column=1, row=1, padx=0, columnspan=2)
 
         self.but3 = tk.Button(self.janela, text='3', width="5", height="2")
-        self.but3.grid(column=1, row=1, padx=0, pady=0, columnspan=2)
+        self.but3.grid(column=2, row=1, padx=0, pady=0, columnspan=2)
 
         self.but4 = tk.Button(self.janela, text='4', width="5", height="2")
-        self.but4.grid(column=0, row=2, padx=0, pady=0)
+        self.but4.grid(column=1, row=2, padx=0, pady=0)
 
         self.but5 = tk.Button(self.janela, text='5', width="5", height="2")
-        self.but5.grid(column=1, row=2, padx=0, pady=0)
+        self.but5.grid(column=2, row=2, padx=0, pady=0)
 
         self.but6 = tk.Button(self.janela, text='6', width="5", height="2")
-        self.but6.grid(column=2, row=2, padx=0, pady=0)
+        self.but6.grid(column=3, row=2, padx=0, pady=0)
+
+        self.janela.columnconfigure(0, weight=1)
+        self.janela.columnconfigure(4, weight=1)
 
 
 janelaPrincipal = tk.Tk()
--- interface/exercicio/atividade_4.py
+++ interface/exercicio/atividade_4.py
@@ -3,13 +3,28 @@ import tkinter as tk
 class Tela:
     def __init__(self, master):
         self.janela = master
-        self.janela.title("Gerenciador Grid")
+        self.janela.title("Soma Simples")
         self.janela.geometry("400x200")
         self.janela.configure(background='grey')
         self.janela.resizable(0, 0)
         self.lbl1 = tk.Label(self.janela, text="Número 1:")
         self.lbl1.grid(column=0, row=0)
+        self.ent1 = tk.Entry(self.janela)
+        self.ent1.grid(column=1, row=0)
 
+        self.lbl2 = tk.Label(self.janela, text="Número 1:")
+        self.lbl2.grid(column=0, row=1)
+        self.ent2 = tk.Entry(self.janela)
+        self.ent2.grid(column=1, row=1)
+
+        def somar():
+            soma = int(self.ent1.get()) + int(self.ent2.get())
+            return soma
+
+        self.but = tk.Button(self.janela, text="Somar>>", command=somar)
+        self.but.grid(column=0, row=2)
+
+        self.lbl3 = tk.Label(self.janela, text=somar())
 
 janelaPrincipal = tk.Tk()
 Tela(janelaPrincipal)
