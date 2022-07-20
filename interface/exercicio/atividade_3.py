e60484cdf9f4db91d7f2ff1e3452e3acbc92efd1
EliasCacau <elias.cacau@sou.ufac.br>
2022-07-20 16:34:10 -0500

    muda

--- interface/exercicio/atividade_3.py
+++ interface/exercicio/atividade_3.py
@@ -4,25 +4,25 @@ class Tela:
     def __init__(self, master):
         self.janela = master
         self.janela.title("Gerenciador Grid")
-        self.janela.geometry("400x230")
+        self.janela.geometry("200x180")
         self.janela.resizable(0, 0)
-        self.but1 = tk.Button(self.janela, text='1')
-        self.but1.grid(column=3, row=0, stick=tk.N,  padx=3, pady=0, rowspan=3)
+        self.but1 = tk.Button(self.janela, text='1', width="5", height="2")
+        self.but1.grid(column=1, row=0,   padx=0, pady=0)
 
-        self.but2 = tk.Button(self.janela, text='2')
-        self.but2.grid(column=2, row=1,stick=tk.N, padx=0, rowspan=3)
+        self.but2 = tk.Button(self.janela, text='2', width="5", height="2")
+        self.but2.grid(column=0, row=1, padx=0, columnspan=2)
 
-        self.but3 = tk.Button(self.janela, text='3')
-        self.but3.grid(column=3, row=1,stick=tk.N, padx=0, pady=0, rowspan=4)
+        self.but3 = tk.Button(self.janela, text='3', width="5", height="2")
+        self.but3.grid(column=1, row=1, padx=0, pady=0, columnspan=2)
 
-        self.but4 = tk.Button(self.janela, text='4')
-        self.but4.grid(column=0, row=2,stick=tk.N, padx=0, pady=0)
+        self.but4 = tk.Button(self.janela, text='4', width="5", height="2")
+        self.but4.grid(column=0, row=2, padx=0, pady=0)
 
-        self.but5 = tk.Button(self.janela, text='5')
-        self.but5.grid(column=1, row=2,stick=tk.N, padx=0, pady=0)
+        self.but5 = tk.Button(self.janela, text='5', width="5", height="2")
+        self.but5.grid(column=1, row=2, padx=0, pady=0)
 
-        self.but6 = tk.Button(self.janela, text='6')
-        self.but6.grid(column=2, row=2,stick=tk.N, padx=0, pady=0)
+        self.but6 = tk.Button(self.janela, text='6', width="5", height="2")
+        self.but6.grid(column=2, row=2, padx=0, pady=0)
 
 
 janelaPrincipal = tk.Tk()
